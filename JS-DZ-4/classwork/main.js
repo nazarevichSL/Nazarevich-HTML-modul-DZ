3b13a5df503e27729c18d18ea5596cdf910257fd
nazarevichSL <sportsila83@gmail.com>
2021-11-22 18:13:06 +0200

    задание JS DZ-4 homework последне.

--- JS-DZ-4/classwork/main.js
+++ JS-DZ-4/classwork/main.js
@@ -112,7 +112,7 @@
 // }
 
 // - створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
-// numbers = [1,4,7];
+// numbers = [1,4,7]; //12/3=4
 // function arr (numbers) {
 //     let summa = 0;
 //     for (const numS of numbers) {
@@ -120,17 +120,85 @@
 //     }
 //     return summa / numbers.length;
 // }
+// document.write(arr(numbers))
 
 // - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше (Math використовувати заборонено);
-function maxminArr (nums) {
-    let min = nums[0]
-    let max = nums[0]
-    for (const num of nums) {
-        if ()
-        
-    }
-}
+// function maxminArr (arg) {
+//     let min = argum[0];
+//     let max = argum[0];
+//     for (const num of argum) {
+//         if (num > max) {
+//             max = num;
+//         }
+//         if (num < min) {
+//             min = num;
+//         }
+//     }
+//     console.log("max", max)
+//     return min;
+// }
+// document.write('Мінімальне число з введених: ' + maxminArr (1,2,6,9,4))
+
+
 // - створити функцію яка заповнює масив рандомними числами
 // (цей код генерує рандомні числа в діапазоні від 0 до 100 - Math.round(Math.random()*100)) та виводить його.
-// - створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit. limit - аргумент, який характеризує кінцеве значення діапазону.
+// function roundMass (arr) {
+//     for (i = 0; i < 10; i++) {
+//     let arr[i]= Math.round(Math.random()*100);
+//     }
+//     return arr
+// }
+// document.write(roundMass(10))
+
+
+// function funRandom (length) {
+//     let arrayEmpty = [];
+//     for (let i = 0; i < length; i++){
+//         arrayEmpty.push(Math.floor(Math.random()*100));
+//     }
+// return arrayEmpty
+// };
+//
+// document.write(funRandom(12))
+
+// - створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit. limit -
+// аргумент, який характеризує кінцеве значення діапазону.
+// function roundMasslimit (arr, limit) {
+//     for (i = 0; i < 10; i++) {
+//     let arr[i]= Math.round(Math.random()*limit);
+//     }
+//     return arr
+// }
+// document.write(roundMasslimit(10,12))
+
+
+// function randomNumber(length,limit) {
+//     let emptyArray = [];
+//     for (let i = 0; i < length; i++) {
+//         emptyArray.push(Math.round(Math.random() * limit));
+//     }
+//     return emptyArray;
+// }
+// document.write(randomNumber(10,100));
+
+
 // - Функція приймає масив та робить з нього новий масив в зворотньому порядку. [1,2,3] -> [3, 2, 1].
+function revers (arr) {
+    let revArr = [];
+    for (let i = arr.length - 1; n = 0; i >= 0; i--; n++) {
+        revArr [n] = revArr [i];
+    }
+    return revArr
+}
+document.write( revers([1,2,3]);
+
+
+// function reverse(arr) {
+//     let arr = [];
+//     for (let i = arr.length - 1, ri = 0; i >= 0; i--, ri++) {
+//         newArr[ri] = newArr[i];
+//     }
+//
+//     return newArr;
+// }
+// document.write( reverse([1,2,3]);

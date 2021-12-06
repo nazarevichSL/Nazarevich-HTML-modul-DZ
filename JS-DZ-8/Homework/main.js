16dd8956ab6e5b999fa21925cc4560806a1853c5
nazarevichSL <sportsila83@gmail.com>
2021-12-06 20:49:03 +0200

    Задание JS-DZ-8 homework пос.

--- JS-DZ-8/Homework/main.js
+++ JS-DZ-8/Homework/main.js
@@ -1,10 +1,35 @@
 // - Напишіть код,  котрий :
 // -- отримує текст з параграфа з id "content"
+// let conText = document.getElementById("content");
+// console.log(conText.innerText);
 
 // -- отримує текст з блоку з id "rules"
+// console.log(document.getElementById('rules').innerText);
+
 // -- замініть текст параграфа з id 'content' на будь-який інший
+// let contentId = document.getElementById("content");
+// contentId.innerHTML = "<b>Lorem dfgd rswdx</b>";
+
 // -- замініть текст параграфа з id 'rules' на будь-який інший
+// let contentRules = document.getElementById('rules');
+// contentRules.innerHTML = "<em>fdgcmgdfhdcvgd gdtghfd rsdgd tedcg rsdfsd fsdc</em>";
+
 // -- змініть кожному елементу колір фону на червоний
+// let backElement = document.body.children;
+// for (const backElementElement of backElement) {
+//     backElementElement.style.background = 'red';
+// }
 // -- змініть кожному елементу колір тексту на синій
+// let bacElement = document.body.children;
+// for (const bacElementElement of bacElement) {
+//     bacElementElement.style.background = 'blue';
+// }
 // -- отримати весь список класів елемента з id=rules і вивести їх в console.log
+// let classRules = document.getElementById('rules').classList;
+// console.log(classRules);
+
 // -- поміняти колір тексту у всіх елементів fc_rules на червоний
+// let colorText = document.getElementsByClassName('fc_rules');
+// for (const colorTextElement of colorText) {
+//     colorTextElement.style.color = 'red';
+}

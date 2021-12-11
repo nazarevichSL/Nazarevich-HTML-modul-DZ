8dbf87dac6b105277df6a2a3792de260c43d3aff
nazarevichSL <sportsila83@gmail.com>
2021-12-11 20:18:11 +0200

    Задание JS-DZ-9 classwork пос.

--- JS-DZ-9/Homework/main.js
+++ JS-DZ-9/Homework/main.js
@@ -1,6 +1,6 @@
 // Все робити за допомоги js.
 // - створити блок,
-//     - додати йому класи wrap, collapse, alpha, beta
+//  - додати йому класи wrap, collapse, alpha, beta
 // - додати стилі(довільні значення) : колір фону, колір тексту, розмір тексту
 // - додати цей блок в body.
 // - клонувати його повністю, та додати клон в body.
@@ -59,20 +59,20 @@
 // За допомоги скріпта для кожного елементу масиву зробити <div class='item'>, в якому буде <h1 class='heading'>
 // з title  елементу, та <p class='description'> з monthDuration елементу.
 //     Завдання робити через цикли.
-// let coursesAndDurationArray = [
-//     {title: 'JavaScript Complex', monthDuration: 5},
-//     {title: 'Java Complex', monthDuration: 6},
-//     {title: 'Python Complex', monthDuration: 6},
-//     {title: 'QA Complex', monthDuration: 4},
-//     {title: 'FullStack', monthDuration: 7},
-//     {title: 'Frontend', monthDuration: 4}
-// ];
-// for (const courseElement of coursesAndDurationArray) {
-//     let div = document.createElement('div');
-//     let h1 = document.createElement('h1');
-//     h1.innerText = courseElement.title;
-//     let p = document.createElement('p');
-//     p.innerText =courseElement.monthDuration;
-//     div.append(h1,p);
-//     document.body.appendChild(div);
-// }
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+for (const courseElement of coursesAndDurationArray) {
+    let div = document.createElement('div');
+    let h1 = document.createElement('h1');
+    h1.innerText = courseElement.title;
+    let p = document.createElement('p');
+    p.innerText =courseElement.monthDuration;
+    div.append(h1,p);
+    document.body.appendChild(div);
+}
